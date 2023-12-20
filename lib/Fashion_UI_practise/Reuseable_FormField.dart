a7005a5c58e69e3dd21cf13a23813c1132a2cbc6
std <mraza.valyani@gmail.com>
2023-12-20 14:02:43 +0500

    Fashion_update With LoggedIn Validation

--- lib/Fashion_UI_practise/Reuseable_FormField.dart
+++ lib/Fashion_UI_practise/Reuseable_FormField.dart
@@ -7,14 +7,22 @@ class FormField1 extends StatelessWidget {
     required this.label_name,
     required this.hint_text,
     required this.pre_icon,
-    this.suff_icon
+    this.suff_icon,
+    required this.Password_Hide,
+
+    required this.textContoller,
+    required this.errorMessage,
 
   }) : super(key: key);
 
   final String label_name;
   final String hint_text;
   final Icon  pre_icon;
-  final Icon?  suff_icon;
+  final IconButton?  suff_icon;
+  final bool Password_Hide;
+
+  final TextEditingController textContoller;
+  final String errorMessage;
 
 
   @override
@@ -22,6 +30,14 @@ class FormField1 extends StatelessWidget {
     return Container(
         width: 400,
         child:TextFormField(
+          controller:textContoller,
+
+          validator: (value){
+            if(value == null || value.isEmpty || value == " "){
+                return errorMessage;
+            }
+          },
+
           decoration: InputDecoration(
             label: Text(label_name),
             hintText: hint_text,
@@ -29,10 +45,13 @@ class FormField1 extends StatelessWidget {
             suffixIcon: suff_icon,
             border: OutlineInputBorder(
                 borderSide: BorderSide(color: Colors.black),
-                borderRadius: BorderRadius.circular(14)
+                borderRadius: BorderRadius.circular(14),
             ),
           ),
-          obscureText: true,
+
+
+
+          obscureText: Password_Hide,
           obscuringCharacter: '*',
         )
     );
--- lib/Fashion_UI_practise/login_page.dart
+++ lib/Fashion_UI_practise/login_page.dart
@@ -10,12 +10,21 @@ class LogInPage extends StatefulWidget {
 }
 
 class _LogInPageState extends State<LogInPage> {
+
+  bool IsHide = true;
+  TextEditingController userName = TextEditingController();
+  TextEditingController userEmail = TextEditingController();
+  TextEditingController userPassword = TextEditingController();
+  final Formkey =GlobalKey<FormState>();
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Color(0xffff9b9b),
+        backgroundColor: const Color(0xffff9b9b),
         elevation: 0,
         title: const Text('Fashion'),
         // actions:  [
@@ -31,36 +40,72 @@ class _LogInPageState extends State<LogInPage> {
         // ],
         automaticallyImplyLeading: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.bottomCenter,
-            child:const Text('Log In',style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,),),
-            width: double.infinity,
-            height: 200,
-            //color: Colors.black,
-          ),
-          const SizedBox(height: 20,),
+      body: Form(
+        key:Formkey,
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child:const Text('Log In',style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,),),
+              width: double.infinity,
+              height: 200,
+              //color: Colors.black,
+            ),
+            const SizedBox(height: 20,),
 
-          const FormField1(
-            label_name: 'Name',
-            hint_text: 'ABC',
-            pre_icon: Icon(Icons.person),
-          ),const SizedBox(height: 5,),
-          const FormField1(
-            label_name: 'Email',
-            hint_text: 'ABC@gmail.com',
-            pre_icon: Icon(Icons.email),
-          ),const SizedBox(height: 5,),
-          const FormField1(
-            label_name: 'Password',
-            hint_text: '*****',
-            pre_icon: Icon(Icons.password),
-          ),
+            FormField1(
+              label_name: 'Name',
+              hint_text: 'ABC',
+              pre_icon: const Icon(Icons.person),
+              Password_Hide: false,
+              errorMessage: 'Enter Your Name',
+              textContoller: userName,
 
+            ),const SizedBox(height: 5,),
+            FormField1(
+              label_name: 'Email',
+              hint_text: 'ABC@gmail.com',
+              pre_icon: const Icon(Icons.email),
+              Password_Hide: false,
+              errorMessage: 'Enter Your Email',
+              textContoller: userEmail ,
+            ),const SizedBox(height: 5,),
 
-        ],
-      ),
+            FormField1(
+              label_name: 'Password',
+              hint_text: '*****',
+              Password_Hide: IsHide==true?true:false,
+              pre_icon: const Icon(Icons.password),
+              suff_icon: IconButton(onPressed: (){
+                setState(() {
+                  IsHide = ! IsHide;
+                });
+              }, icon: IsHide == true? const Icon(Icons.remove_red_eye_rounded):const Icon(Icons.remove_red_eye_outlined),),
+
+              errorMessage: 'Enter Your Password',
+              textContoller: userPassword,
+            ),
+
+            const SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){
+              if(Formkey.currentState!.validate()){
+                debugPrint(userName.text.toString());
+                debugPrint(userEmail.text.toString());
+                debugPrint(userPassword.text.toString());
+                userName.clear();
+                userEmail.clear();
+                userPassword.clear();
+
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(
+                  content: Text('${userName.text.toString()} Logged In'),
+                ));
+              }
+            }, child: const Text('LogIn'))
+
+          ],
+        ),
+      )
     );
   }
 }
