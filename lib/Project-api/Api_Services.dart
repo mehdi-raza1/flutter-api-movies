7d7bced98853ab21110ed75e96ef9aab0f9f5344
std <mraza.valyani@gmail.com>
2023-12-28 14:10:25 +0500

    api_movies

---
+++ lib/Project-api/Api_Services.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class ApiServices{
+
+ static Future futureApi()async{
+   var request = http.Request('GET', Uri.parse('https://www.episodate.com/api/most-popular?page=1'));
+
+
+  http.StreamedResponse response = await request.send();
+
+
+  if (response.statusCode == 200) {
+    var res = await response.stream.bytesToString();
+    debugPrint(res);
+    return res;
+  }
+  else {
+  debugPrint(response.reasonPhrase);
+  return null;
+  }
+
+}
+}
--- lib/Project-api/apiscreen.dart
+++ lib/Project-api/apiscreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:mehdifirstfast/Project-api/Api_Services.dart';
 
 import 'movieStack_reusable.dart';
 
@@ -15,16 +18,44 @@ class _apiscreen_movieState extends State<apiscreen_movie> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-        return const movieStack(
-          image: 'images/boy.png',
-          MovieName: 'Hello WOrld',
-          startDate: '20/3/2020',
-          network: 'bye bye' ,
-          Status: 'DOne',
-        );
+      body: FutureBuilder(
+        future: ApiServices.futureApi(),
+        builder: (context, snapshot) {
+
+        if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator(),);
+        }
+
+        if(snapshot.hasData){
+          Map map = jsonDecode("${snapshot.data}");
+          List myData = map["tv_shows"];
+
+          return ListView.builder(
+            itemCount:  myData.length,
+              itemBuilder: (context, index) {
+
+              String movieName =  myData[index]["name"];
+              String movieImage =  myData[index]["image_thumbnail_path"];
+              String movieNetwork =  myData[index]["network"];
+              String movieDate =  myData[index]["start_date"];
+              String movieStatus =  myData[index]["status"];
+
+                return movieStack(
+                      image: movieImage,
+                      MovieName: movieName,
+                      startDate: movieNetwork,
+                      network: movieDate,
+                      Status: movieStatus,
+                    );
+                  },);
+
+        }
+
+        if(snapshot.hasError){
+          return Icon(Icons.error,color: Colors.red,);
+        }
+
+        return Container();
       },)
     );
   }
